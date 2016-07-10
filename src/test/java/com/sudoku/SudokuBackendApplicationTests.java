24e944ffde67edbb8e733186ec14fd2175db3334
Felipe Cardozo <pypelyncar@hotmail.com>
2016-07-10 13:06:19 +0200

    first commit

---
+++ src/test/java/com/sudoku/SudokuBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sudoku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SudokuBackendApplication.class)
+public class SudokuBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
